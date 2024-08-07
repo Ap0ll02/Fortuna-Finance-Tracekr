d45e9cad68c6448415e5f9a1086979471f146559
Ap0ll0 <Ap0ll0Media@protonmail.com>
2024-08-07 16:16:00 -0700

    Small Update
    To resume user input into add transaction

--- my_finance_tracker/src/components/transaction.jsx
+++ my_finance_tracker/src/components/transaction.jsx
@@ -2,12 +2,21 @@ import React, {useState} from 'react'
 import { invoke } from '@tauri-apps/api/tauri'
 import "../App.css";
 const Transaction = () => {
+    const [desc, setDesc] = useState('');
+    const [amt, setAmt] = useState('');
+    const [dt, setDt] = useState('');
     const [lastTransaction, setLastTransaction] = useState(null);
-    const handleClick = async () => {
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      handleClick(desc, amt, dt);
+    };
+
+    const handleClick = async (desc, amt, dt) => {
         try {
-            const description = "Lunch";
-            const amount = 12.50;
-            const date = "07/08/2024";
+            const description = desc;
+            const amount = amt;
+            const date = dt;
 
             const response = await invoke('add_transaction', { description, amount, date });
             console.log(response);
@@ -19,10 +28,23 @@ const Transaction = () => {
 
   return (
     <div>
-      <button onClick={handleClick} className='t_main'>Add A Transaction</button>
       {lastTransaction && (
-        <p id="last_t">Last Transaction: {lastTransaction.description}, ${lastTransaction.amount} on {lastTransaction.date}</p>
+        <p className="desc">Last Transaction: {lastTransaction.description}, ${lastTransaction.amount} on {lastTransaction.date}</p>
       )}
+      <form>
+        <label className='desc'>Input Transaction Name (ex: Morning coffee):</label>
+        <br></br>
+        <input className='black_btn' type="text" value={desc} onChange={(e) => setDesc(e.target.value)}></input>
+        <br></br>
+        <label className='desc'>Input Transaction Amount (ex: 5.99):</label>
+        <br></br>
+        <input className='black_btn' type="text" value={amt} onChange={(e) => setAmt(e.target.value)}></input>
+        <br></br>
+        <label className='desc' value={dt} onChange={(e) => setDt(e.target.value)}>Input Transaction Date (DD/MM/YEAR):</label>
+        <br></br>
+        <input className='black_btn' type="text"></input><br></br>
+        <input className="black_btn" type="submit" value="Add Transaction" onSubmit={handleSubmit}></input>
+      </form>
     </div>
   );
 };
